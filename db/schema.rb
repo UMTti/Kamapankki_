5a0459d8c83777b8ebdca229e796f5cb1a537996
Pihla Toivanen <toivanenpihla@gmail.com>
2015-05-30 18:20:18 +0300

    pg

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,10 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140525093546) do
+ActiveRecord::Schema.define(version: 20140524161719) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
 
   create_table "groups", force: true do |t|
     t.string   "name"
@@ -19,7 +22,6 @@ ActiveRecord::Schema.define(version: 20140525093546) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "description"
-    t.integer  "message_id"
   end
 
   create_table "groups_users", id: false, force: true do |t|
@@ -35,7 +37,6 @@ ActiveRecord::Schema.define(version: 20140525093546) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.integer  "stuff_id"
-    t.integer  "group_id"
   end
 
   create_table "stuffs", force: true do |t|
